c7f70b0822eee186fb9a408ccb2308276aa57c46
puyomi <rex0912@naver.com>
2018-11-06 01:04:52 +0900

    commit init

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./styles.scss";
 
 class App extends Component {
   render() {
-    return <div className="App">APP</div>;
+    return <div className="App">두렌트</div>;
   }
 }
 
