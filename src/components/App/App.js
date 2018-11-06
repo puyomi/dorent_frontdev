6459b35ab52dee2424d87782e3c2add240dc8324
puyomi <rex0912@naver.com>
2018-11-07 00:19:31 +0900

    404 page done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,30 @@
-import React, { Component } from "react";
+import React, { Component }  from "react";
 import "./styles.scss";
-import Navigation from "components/Navigation"
+import Navigation from "components/Navigation";
 import { Switch, Route } from "react-router-dom";
 
-const App = () => [
-  <Navigation key={2} />,
-  <MainFrame key={4} />,
-  // <Footer key={5} />
-];
+class App extends Component {
+  render() {
+    return [
+      <Navigation key={2} />,
+      <MainFrame key={4} />
+      // <Footer key={5} />
+    ];
+  }
+}
 
 const MainFrame = () => (
   <Switch>
-    <Route exact path="/" render={()=>"메인화면"} />
-    <Route exact path="/products" render={()=>"상품화면"} />
-    <Route exact path="/upload" render={()=>"업로드화면"} />
-    <Route exact path="/auth" render={()=>"로그인화면"} />
+    <Route exact path="/" render={() => "메인화면"} />
+    <Route exact path="/products" render={() => "상품화면"} />
+    <Route exact path="/upload" render={() => "업로드화면"} />
+    <Route exact path="/auth" render={() => "로그인화면"} />
+    <Route component={NotFound} />
   </Switch>
 );
 
+const NotFound = () => (
+  <div>404! Page NOT FOUND</div>
+)
+
 export default App;
-
-
