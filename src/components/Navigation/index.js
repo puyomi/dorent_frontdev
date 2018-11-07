0d54b90c9e6039040967e82293eb999100c58055
puyomi <rex0912@naver.com>
2018-11-07 21:12:43 +0900

    nav onworking

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,28 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Ionicon from "react-ionicons";
+import styles from "./styles.module.scss";
 
-const Navigation = props => {
+const Navigation = () => {
   return (
-    <nav>
-      <Link to="/">
-        <span>로고 |</span>
-      </Link>
-      <Link to="/products">
-        <span>상품화면 |</span>
-      </Link>
-      <Link to="/upload">
-        <span>
-          <Ionicon icon="md-camera" fontSize="18px" />
-          물건 등록하기 |
-        </span>
-      </Link>
-      <Link to="/auth">
-        <span>
-          <Ionicon icon="md-person" fontSize="18px" />
-          로그인
-        </span>
-      </Link>
+    <nav className={styles.navigation}>
+      <div className={styles.inner}>
+        <Link to="/">
+          <span>로고 |</span>
+        </Link>
+        <Link to="/products">
+          <span>상품화면 |</span>
+        </Link>
+        <Link to="/upload">
+          <span>
+            <Ionicon icon="md-camera" fontSize="18px" />
+            물건 등록하기 |
+          </span>
+        </Link>
+        <Link to="/auth">
+          <span>
+            <Ionicon icon="md-person" fontSize="18px" />
+            로그인
+          </span>
+        </Link>
+      </div>
     </nav>
   );
 };
